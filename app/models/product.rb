135427c9eb9afd88d5fee768dbc3d5bb99610bbc
Rodion Grishaev <rodion.grishaev@thebigknow.com>
2019-01-28 19:30:37 +0300

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: {message: 'Should be unique.'}, length: {maximum: 20, message: 'Maximum 20 characters.'}
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
+  }
 end
