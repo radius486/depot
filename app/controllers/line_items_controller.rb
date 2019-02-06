cd0d53a1d219ae82557b2f5409205f9b5e7e2ada
Rodion Grishaev <rodion.grishaev@thebigknow.com>
2019-02-06 17:12:01 +0300

    Extended remove method for line-items.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -57,6 +57,8 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
+    @line_items = LineItem.all
+
     if @line_item.quantity > 1
       @line_item.quantity -= 1
       @line_item.save!
@@ -64,9 +66,18 @@ class LineItemsController < ApplicationController
       @line_item.destroy
     end
 
-    respond_to do |format|
-      format.html { redirect_to @line_item.cart, notice: 'Line item was successfully destroyed.' }
-      format.json { head :no_content }
+    if @line_items.length == 0
+      @line_item.cart.destroy if @line_item.cart.id == session[:cart_id]
+      session[:cart_id] = nil
+      respond_to do |format|
+        format.html { redirect_to store_url, notice: 'Your cart is currently empty' }
+        format.json { head :no_content }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully destroyed.' }
+        format.json { head :no_content }
+      end
     end
   end
 
