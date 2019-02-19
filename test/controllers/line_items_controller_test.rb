e8baf7cd6e58a97e640d6b2c25ebb7b1e3410fad
Rodion Grishaev <rodion.grishaev@thebigknow.com>
2019-02-19 19:08:22 +0300

    Fixing tests.

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -20,7 +20,20 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
       post line_items_url, params: {product_id: products(:ruby).id}
     end
 
-    assert_redirected_to cart_path(assigns(:line_item).cart)
+    assert_redirected_to store_path
+  end
+
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      #xhr :post, :create, product_id: products(:ruby).id
+      post line_items_url, xhr: true, params: {
+        product_id: products(:ruby).id
+      }
+    end
+    assert_response :success
+    assert_select_jquery :html, '#cart' do
+      assert_select 'tr.current_item td', /Programming Ruby 2.4/
+    end
   end
 
   test "should show line_item" do
@@ -43,6 +56,6 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
       delete line_item_url(@line_item)
     end
 
-    assert_redirected_to line_items_url
+    assert_redirected_to store_path
   end
 end
